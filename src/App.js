b2678603f5f81c8f6a41ba6a6a03f4a5bdffd7e0
zuqueto200 <ricardozuqueto@gmail.com>
2022-05-20 21:48:33 -0300

    localStorage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
+
+
+export function App() {
+
+  const [users, setUsers] = useState(JSON.parse(localStorage.getItem('users')))
+
+
+  function FnBt(e) {
+
+    const email = document.getElementById('email').value
+    const senha = document.getElementById('pass').value
+
+    var validacao = true
+
+    if (localStorage.getItem('users') !== 'null') {
+
+      users.map((e) => {
+        if (e.email === email) {
+          validacao = false
+        }
+      })
+
+    } else {
+
+      if (validacao === true && email !== '' && senha !== '') {
+
+        setUsers([{
+
+          email: email,
+          senha: senha
+        }])
+
+      }
+    }
+
+
+    if (validacao === true && email !== '' && senha !== '') {
+
+      setUsers([...users, {
+        email: email,
+        senha: senha
+      }])
+
+    }
+
+  }
+
+
+
+
+  useEffect(() => {
+
+    localStorage.setItem('users', JSON.stringify(users))
+
+  }, [users])
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centralizar">
+
+      <div className="divInput">
+
+          <h3>ARMAZENAMENTO EM LOCAL STORAGE</h3>
+        <label >
+          Email
+          <input id="email" type='email' />
+        </label>
+
+        <label>
+          Senha
+          <input id="pass" type='password' />
+        </label>
+
+        <button id="btCadastrar" onClick={(e) => { FnBt(e) }}>CADASTRAR</button>
+
+        <div className="listando">
+
+          {users && (users.map((e, i) => (
+            <div className="linha" key={i}>
+
+
+              <div className="divEmailSenha">
+
+                <div className={"email" + (i%2==0?" l1":" l2")} >
+
+                  {e.email}
+
+                </div>
+
+
+                <div className={"senha" + (i%2==0?" l1":" l2")}>
+
+                  {e.senha}
+
+                </div>
+                <div className="divBtDel">
+
+                  <button className={"del" + (i%2==0?" l1":" l2")} onClick={() => { setUsers(users.filter((item, index) => index !== i)) }}>x</button>
+              </div>
+              </div>
+            </div>
+
+          )))}
+
+
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+
